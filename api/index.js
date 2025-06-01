a2b68bb6b73043147353d89ca5ad48372966a0cb
Akram Hossain <dhaliakram6@gmail.com>
2025-06-02 01:38:46 +0600

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -26,6 +27,7 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRoutes);//akhan theke jabe user.route.js file ae 
 app.use('/api/auth',authRoutes);//akhan theke jabe auth.route.js file ae 
+app.use('/api/post',postRoutes);//akhan theke jabe post.route.js file ae 
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
